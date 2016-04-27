eb394d4ebca62f4fa192ed2cb889bb67f560e794
olivier R-D <olivier@loke>
2016-04-27 15:15:54 +0200

    require math3d now that is available on pip

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.command.install_data import install_data
 
 
 setup (name = "urx", 
-        version = "0.9.4",
+        version = "0.10.0",
         description = "Python library to control an UR robot",
         author = "Olivier Roulet-Dubonnet",
         author_email = "olivier.roulet@gmail.com",
         url = 'https://github.com/oroulet/python-urx',
         packages = ["urx"],
         provides = ["urx"],
+        install_requires=["math3d"],
         license = "GNU General Public License v3",
 
         classifiers = [
