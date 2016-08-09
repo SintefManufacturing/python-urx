b6eb54566d8f303800a606ea0aba397e81d7ad78
Alvaro Capellan <alvaro.capellan@sintef.no>
2016-08-09 11:38:47 +0200

    Parse MasterBoardData correctly for all firmware versions

--- urx/ursecmon.py
+++ urx/ursecmon.py
@@ -88,8 +88,12 @@ class ParserUtils(object):
             elif ptype == 5:
                 allData["LaserPointer(OBSOLETE)"] = self._get_data(pdata, "iBddd", ("size", "type"))
             elif ptype == 3:
-                #fmt = "iBhhbbddbbddffffBBb"    # firmware 1.6, 1.7, 1.8
-                fmt = "iBiibbddbbddffffBBb"     # firmware 3.0, 3.1, 3.2
+
+                if self.version >= (3, 0):
+                    fmt = "iBiibbddbbddffffBBb"     # firmware >= 3.0
+                else:
+                    fmt = "iBhhbbddbbddffffBBb"     # firmware < 3.0
+
                 allData["MasterBoardData"] = self._get_data(pdata, fmt, ("size", "type", "digitalInputBits", "digitalOutputBits", "analogInputRange0", "analogInputRange1", "analogInput0", "analogInput1", "analogInputDomain0", "analogInputDomain1", "analogOutput0", "analogOutput1", "masterBoardTemperature", "robotVoltage48V", "robotCurrent", "masterIOCurrent"))  # , "masterSafetyState" ,"masterOnOffState", "euromap67InterfaceInstalled"   )) 
             elif ptype == 2:
                 allData["ToolData"] = self._get_data(pdata, "iBbbddfBffB", ("size", "type", "analoginputRange2", "analoginputRange3", "analogInput2", "analogInput3", "toolVoltage48V", "toolOutputVoltage", "toolCurrent", "toolTemperature", "toolMode"))
