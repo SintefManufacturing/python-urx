372b591860411add180dec21f1f22dfafb661f67
Robin Modisch <modisch.fabrications@gmail.com>
2020-11-04 18:45:41 +0100

    update min_t to t
    see https://github.com/SintefManufacturing/python-urx/issues/73#issuecomment-555387672 for the hotfix. This might limit usage to UR > 3.5

--- urx/urrobot.py
+++ urx/urrobot.py
@@ -314,7 +314,7 @@ class URRobot(object):
         vels = [round(i, self.max_float_length) for i in velocities]
         vels.append(acc)
         vels.append(min_time)
-        prog = "{}([{},{},{},{},{},{}], a={}, t_min={})".format(command, *vels)
+        prog = "{}([{},{},{},{},{},{}], a={}, t={})".format(command, *vels)
         self.send_program(prog)
 
     def movej(self, joints, acc=0.1, vel=0.05, wait=True, relative=False, threshold=None):
